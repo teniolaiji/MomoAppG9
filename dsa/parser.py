7eda207f441dc339b00c3a6a9b1ee16105cb93c7
t-iji7 <o.iji@alustudent.com>
2025-10-01 18:18:50 +0200

    regex complete

---
+++ dsa/parser.py
@@ -0,0 +1,21 @@
+import xml.etree.ElementTree as ET
+import re
+
+# path to the xml file
+
+
+def parse_transactions(raw_xml_data_path):
+    tree = ET.parse(raw_xml_data_path)
+    root = tree.getroot()
+
+# regex to extract specified fields
+    AIRTIME_OR_UTILITIES = re.compile(r"(\d{1,3}(?:,\d{3})*|\d+) RWF")
+    MOMO_IN = re.compile(r"received (\d+) RWF from (.+?) \(")
+    MOMO_OUT = re.compile(
+        r"(\d{1,3}(?:,\d{3})*|\d+) RWF transferred to (.+?) \(")
+    MERCHANT_PAYMENT = re.compile(
+        r"payment of (\d{1,3}(?:,\d{3})*|\d+) RWF to (.+?) ")
+    BANK_DEPOSIT = re.compile(r"deposit of (\d+) RWF")
+    AGENT_WITHDRAWAL = re.compile(r"withdrawn (\d+) RWF.*?via agent: (.+?) ")
+    BUSINESS_PAYMENT = re.compile(r"transaction of (\d+) RWF by (.+?) ")
+    timestamp_pattern = re.compile(r"at (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})")
