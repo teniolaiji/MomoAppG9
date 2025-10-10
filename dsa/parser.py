8d4645a40327435a092b6cf9c41389d7d3267f64
Oluwaloseyitowi Iji <teniolaiji@gmail.com>
2025-10-10 07:39:41 +0200

    Update parser.py

--- dsa/parser.py
+++ dsa/parser.py
@@ -1,14 +1,11 @@
 import xml.etree.ElementTree as ET
 import re
 
-# path to the xml file
-
-
 def parse_transactions(raw_xml_data_path):
     tree = ET.parse(raw_xml_data_path)
     root = tree.getroot()
 
-# regex to extract specified fields
+# regex for specified fields
     AIRTIME_OR_UTILITIES = re.compile(r"(\d{1,3}(?:,\d{3})*|\d+) RWF")
     MOMO_IN = re.compile(r"received (\d+) RWF from (.+?) \(")
     MOMO_OUT = re.compile(
@@ -20,11 +17,11 @@ def parse_transactions(raw_xml_data_path):
     BUSINESS_PAYMENT = re.compile(r"transaction of (\d+) RWF by (.+?) ")
     timestamp_pattern = re.compile(r"at (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})")
 
-# initialize list to store all parsed records as dictionaries
+# list to store all parsed records as dictionaries
     transactions = []
     transaction_id = 1
 
-# loop through sms record
+
     for sms in root:
 
         body = sms.attrib.get('body')
